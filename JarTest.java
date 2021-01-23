99267fe852f07bf982b8ae17ded248ed5eff0311
zimefaza <77891854+zimefaza@users.noreply.github.com>
2021-01-23 20:04:54 +0300

    Add files via upload

---
+++ JarTest.java
@@ -0,0 +1,13 @@
+package com.test;
+
+public class JarTest {
+//	public static void main(String args[]) {
+//		System.out.println("Entered value: " + args[0]);
+//	}
+	
+	public String showString(String arg) {
+		System.out.println("Received " + arg);
+		return "Hello World " + arg;
+	}
+
+}
